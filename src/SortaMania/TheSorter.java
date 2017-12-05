fe8eeba39d0234792e2a320c53d71eb9cc3422b1
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-12-05 12:21:36 -0500

    test test

--- src/SortaMania/TheSorter.java
+++ src/SortaMania/TheSorter.java
@@ -1,10 +1,19 @@
 package SortaMania;
 
+import java.util.Random;
+
 public class TheSorter extends SortCompetition{
 
 	public int challengeOne(int[] arr) {
-		// TODO Auto-generated method stub
-		return 0;
+		selectionSort(arr);
+		if(arr.length%2==1)
+		{
+			return arr[(arr.length)/2];
+		}
+		else
+		{
+			return (arr[(arr.length/2)] + arr[arr.length/2-1])/2;
+		}
 	}
 
 	@Override
@@ -36,4 +45,35 @@ public class TheSorter extends SortCompetition{
 		return null;
 	}
 
+	public static void selectionSort(int[] list1)
+	{
+		for (int i = 0; i < list1.length-1; i++)
+		{
+			for (int j = i+1; j < list1.length; j++)
+			{
+				if (list1[i] > list1[j])
+				{
+					swap(list1,i,j);
+				}
+			}
+		} 
+	}
+
+	public static void swap(int[] arr, int a, int b)
+	{
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+	public static int[] GenerateArr()
+	{
+		int[] name = new int[10000];
+		Random rand = new Random();
+		int x = rand.nextInt(10000) + 1;
+		for(int i = 0; i<name.length;i++)
+		{
+			name[i] = x;
+		}
+		return name;
+	}
 }
